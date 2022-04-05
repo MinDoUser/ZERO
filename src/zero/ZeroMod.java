7a960f2e7986253f67893ca3f0bfc8e6973cc2df
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-04-05 20:23:47 +0200

    Create ZeroMod.java

---
+++ src/zero/ZeroMod.java
@@ -0,0 +1,42 @@
+package zero
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+import mindustry.ui.dialogs.BaseDialog;
+import mindustry.ui.Styles;
+//Yep, it's not the best name, but everthing else just didn't work...
+public class ZeroMod extends Mod{
+  public ZeroMod(){
+    startScreen();
+  }
+	public static void startScreen(){
+		BaseDialog dialog = new BaseDialog("Welcome");
+		dialog.closeOnBack();
+		dialog.cont.pane(inner -> {
+			inner.pane(table -> {
+				table.pane(p -> {
+					p.add("[white]<<< Z E R O >>>", Styles.techLabel).row();
+				}).fillY().growX().row();
+				table.add("").row();
+			}).growX().center().row();
+			
+			inner.table(table -> {
+				table.table(t -> {
+					t.add("$dialogs.welcomeText");
+					t.row();
+					t.button("< Ready >", dialog::hide).size(150f, 100f);
+				}).grow();
+			}).fill();
+		}).grow();
+	dialog.show();
+}
+@Override
+public void loadContent(){
+        Log.info("Loaded all Java files.");
+    }
+  
