6dbc4b95bf33801236d030ec93da390307e49a02
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-04-05 19:52:37 +0200

    Create ZeroMod.java

---
+++ scr/zero/ZeroMod.java
@@ -0,0 +1,22 @@
+package zero;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+
+public class ZeroMod extends Mod{
+
+    public ExampleJavaMod(){
+        Log.info("Welcome to Z E R O!");
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+
+}
