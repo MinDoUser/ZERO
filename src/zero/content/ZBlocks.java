998b3d81d39bf0e56cf0598a7160e86b4a54127e
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-04-08 21:51:13 +0200

    Update ZBlocks.java

--- src/zero/content/ZBlocks.java
+++ src/zero/content/ZBlocks.java
@@ -45,7 +45,7 @@ public class ZBlocks implements ContentList{
 			{
 				requirements(Category.crafting, with(Items.copper, 260, Items.silicon, 43, Items.graphite, 160));
 				craftEffect = Fx.smeltsmoke;
-				ItemStack[] out = new ItemStack(ZItems.steel, 2), new ItemStack(Items.silicon, 1);
+				ItemStack[] out = {new ItemStack(ZItems.steel, 2), new ItemStack(Items.silicon, 1)};
 				outputItems = out;
 				craftTime = 56f;
 				size = 2;
