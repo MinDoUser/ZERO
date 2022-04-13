d92140d201a37ab0ff16162bbb019f04b3956d9e
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-04-13 18:26:10 +0200

    Create ZContent.java

---
+++ src/zero/content/ZContent.java
@@ -0,0 +1,17 @@
+package zero.content;
+
+import arc.Core;
+import arc.func.Cons;
+import arc.graphics.Texture;
+import arc.graphics.g2d.TextureRegion;
+import mindustry.ctype.*;
+import zero.ZeroMod;
+
+public class ZContent implements ContentList{
+  //UI for dialogs.
+  	public static TextureRegion logo;
+  @Override
+  public void load(){
+    logo = Core.atlas.find(ZeroMod.name("upgrade"));
+  }
+}
