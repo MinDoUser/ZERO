a5e884457356d877e126b0dff7c29bb81b86f491
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-05-31 18:51:46 +0200

    Update AboutModDialog.java

--- src/zero/ui/dialogs/AboutModDialog.java
+++ src/zero/ui/dialogs/AboutModDialog.java
@@ -9,6 +9,9 @@ import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.ui.Styles;
 import mindustry.ui.dialogs.BaseDialog;
+import mindustry.ui.*;
+
+import zero.content.*;
 
 public class AboutModDialog extends BaseDialog{
 	public AboutModDialog(){
@@ -34,7 +37,7 @@ public class AboutModDialog extends BaseDialog{
 					t.add("A mod made by: lol02\nIt adds some new blocks and features.\n\n I hope you like them!");
 					t.row();
 					t.row();
-					t.button("< Ok! >", dialog::hide).size(200f, 100f);
+					t.button("< Ok! >", this::hide).size(200f, 100f);
 				}).grow();
 			}).fill();
 		}).grow();
